e96997d537405f1503b990f0cd28c8c373ca0edd
Phạm Mạnh Tùng <tungfpm@gmail.com>
2016-03-04 19:42:09 +0700

    menu game

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @Project NUKEVIET 3.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2016 VINADES.,JSC. All rights reserved
+ * @Createdate Fri, 04 Mar 2016 10:48:30 GMT
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+    die( 'Stop!!!' );
+}
+
+$db_config['dbhost'] = "localhost";
+$db_config['dbport'] = "";
+$db_config['dbname'] = "download";
+$db_config['dbuname'] = "root";
+$db_config['dbpass'] = "";
+$db_config['prefix'] = "nv3";
+
+$global_config['sitekey'] = "9dbce8b47a4b5426e84d852d2bfe9651";// Do not change sitekey!
+
+?>
---
+++ modules/banners/forms/form_random.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Project NUKEVIET 3.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2012 VINADES.,JSC. All rights reserved
+ * @Createdate 3/22/2010 2:34
+ */
+
+
+
+?>
